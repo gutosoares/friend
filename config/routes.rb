829b7e8692073812f8f8cf54032cf1b38c9a9859
Augusto Soares <gutosoares@outlook.com>
2016-07-10 22:31:23 -0300

    Add assocition between Group and User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :groups
-  devise_for :users
   root 'welcome#index'
   
+  resources :groups
+  devise_for :users
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710210203) do
+ActiveRecord::Schema.define(version: 20160711010304) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -33,9 +33,11 @@ ActiveRecord::Schema.define(version: 20160710210203) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.integer  "group_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["group_id"], name: "index_users_on_group_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
